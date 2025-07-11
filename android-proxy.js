c4c9048800249d46c27a57028c4cd4d9514e34fc
Lelonio <emanuelemaio18@gmail.com>
2025-07-11 11:23:48 +0200

    Add files via upload

--- android-proxy.js
+++ android-proxy.js
@@ -6,7 +6,7 @@ import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-const puppeteer = require('puppeteer-core');
+import puppeteer from 'puppeteer-core';
 import express from 'express';
 import fetch from 'node-fetch';
 import https from 'https';
