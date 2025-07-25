42eb837a7e3af28cbb45eebe46281122674f7280
Lelonio <emanuelemaio18@gmail.com>
2025-07-25 17:57:54 +0200

    Update android-proxy.js

--- android-proxy.js
+++ android-proxy.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-const activeStreams = new Map();
+const activeConnections = new Map();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
